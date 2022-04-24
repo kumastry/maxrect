1e070b1695489cb057bdd8cf048788112e8e41c8
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2022-04-25 00:22:48 +0900

    fixed

--- maxrect.py
+++ maxrect.py
@@ -4,10 +4,6 @@ def d(S, T, F):
     res = 0
     for i in S:
         for j in T:
-            print("####")
-            print(i)
-            print(j)
-            print("$$$$")
             if(F[i][j] == 1):
                 res += 1
     res -= S.size + T.size
@@ -18,7 +14,7 @@ def d(S, T, F):
 def main():
     U = np.array([0, 1, 2], dtype=int)
     L = np.array([0, 1, 2, 3, 4], dtype=int)
-    E = np.array([[1, 1, 1, 1, 0],[1, 1, 1, 1, 1], [0, 0, 0, 1, 1]], dtype=int)
+    E = np.array([[1, 1, 1, 0, 0],[1, 1, 1, 1, 1], [0, 0, 0, 1, 1]], dtype=int)
     ec = np.array([],dtype=int)
 
     F = np.copy(E)
@@ -36,16 +32,23 @@ def main():
 
         ##Rの頂点セットからRに連結しているエッジ数が多い順にソートする
         R = sorted(R, reverse=True,key=lambda x:np.sum(F, axis=1)[x])
-
+        ##print(R)
 
         for u in R:
             S = np.append(S, u)
+            T_copy = np.copy(T);
             T = np.array([], dtype=int)
             cnt = 0
             for tail in E[u]:
                 if(tail == 1):
                     T = np.append(T, L[cnt])
                     cnt += 1
+
+            print(k+1)
+            print(S)
+            print(T)
+            print(d(S, T, F)) 
+            print("###")
 
             if(d(S, T, F) > d(S_max, T_max, F)):
                 S_max = np.copy(S)
@@ -57,12 +60,14 @@ def main():
                 for i in S_max:
                     for j in T_max:
                         F[i][j] = 0
-
                 ec = np.append(ec, [S_max, T_max])
                 R = np.copy(U)
             else:
                 R = np.delete(R, 0)
+            
 
+
+            
             if(R.size == 0):
                 break
     
