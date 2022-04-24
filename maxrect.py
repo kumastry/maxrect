1a49e713bd064b876e0923781a89850287d78810
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2022-04-24 23:23:21 +0900

    fixed

--- maxrect.py
+++ maxrect.py
@@ -1,19 +1,33 @@
 import numpy as np
 
-def d(S, T):
-    return 1
+def d(S, T, F):
+    res = 0
+    for i in S:
+        for j in T:
+            print("####")
+            print(i)
+            print(j)
+            print("$$$$")
+            if(F[i][j] == 1):
+                res += 1
+    res -= S.size + T.size
+
+    return res
+
 
 def main():
-    U = np.array([0, 1, 2])
-    L = np.array([0, 1, 2, 3, 4])
-    E = np.array([[1, 1, 1, 1, 0],[1, 1, 1, 1, 1], [0, 0, 0, 1, 1]])
-    ec = np.array([])
+    U = np.array([0, 1, 2], dtype=int)
+    L = np.array([0, 1, 2, 3, 4], dtype=int)
+    E = np.array([[1, 1, 1, 1, 0],[1, 1, 1, 1, 1], [0, 0, 0, 1, 1]], dtype=int)
+    ec = np.array([],dtype=int)
 
     F = np.copy(E)
     R = np.copy(U)
 
+ 
+
     for k in range(10):
-        S = np.array([])
+        S = np.array([], dtype=int)
         T = np.copy(L)
 
         S_max = np.copy(S)
@@ -21,30 +35,35 @@ def main():
 
 
         ##Rの頂点セットからRに連結しているエッジ数が多い順にソートする
-        R = sorted(R)
+        R = sorted(R, reverse=True,key=lambda x:np.sum(F, axis=1)[x])
 
 
         for u in R:
             S = np.append(S, u)
-            T = np.array([])
+            T = np.array([], dtype=int)
             cnt = 0
             for tail in E[u]:
                 if(tail == 1):
                     T = np.append(T, L[cnt])
                     cnt += 1
 
-            if(d(S, T) > d(S_max, T_max)):
+            if(d(S, T, F) > d(S_max, T_max, F)):
                 S_max = np.copy(S)
                 T_max = np.copy(T)
             
-            if(d(S_max, T_max) >= 0):
-                F = F / S_max * T_max
-                ec = ec.append([S_max, T_max])
+            if(d(S_max, T_max, F) >= 0):
+
+                #Fから完全二部グラフS_max × T_maxのエッジを削除
+                for i in S_max:
+                    for j in T_max:
+                        F[i][j] = 0
+
+                ec = np.append(ec, [S_max, T_max])
                 R = np.copy(U)
             else:
                 R = np.delete(R, 0)
 
-            if(R.size() == 0):
+            if(R.size == 0):
                 break
     
     print(ec)
